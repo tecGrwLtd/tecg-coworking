5b8dcd4101821c10a8c828d3e116cb0d186c5f7c
Irenee <iradukundairenee9@gmail.com>
2025-12-08 22:00:54 +0200

    work on feedabck

--- dist/dev/static/chunks/src_app_page_tsx_c1b59eb4._.js
+++ dist/dev/static/chunks/src_app_page_tsx_c1b59eb4._.js
@@ -9,16 +9,16 @@
   "static/chunks/node_modules_antd_es_form_64a98d85._.js",
   "static/chunks/node_modules_antd_es_input_d9703232._.js",
   "static/chunks/node_modules_antd_es_modal_e17af4eb._.js",
-  "static/chunks/node_modules_antd_es_21d90c86._.js",
+  "static/chunks/node_modules_antd_es_335843f5._.js",
   "static/chunks/node_modules_@ant-design_cssinjs_es_a95c0ffe._.js",
   "static/chunks/node_modules_@rc-component_picker_es_07ab3cfc._.js",
   "static/chunks/node_modules_@rc-component_form_es_dcd5743f._.js",
   "static/chunks/node_modules_@rc-component_select_es_ed3847c4._.js",
   "static/chunks/node_modules_@rc-component_trigger_es_20355146._.js",
   "static/chunks/node_modules_@firebase_33585e93._.js",
-  "static/chunks/node_modules_@rc-component_4863bb0d._.js",
+  "static/chunks/node_modules_@rc-component_9742e3fd._.js",
   "static/chunks/node_modules_@ant-design_bd195eb6._.js",
-  "static/chunks/node_modules_1206aae0._.js"
+  "static/chunks/node_modules_683780f1._.js"
 ],
     source: "dynamic"
 });
